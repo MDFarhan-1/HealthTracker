2de1ba39a1c11dce21228c5e7b97d8acc46683e4
MDFarhan-1 <153450239+MDFarhan-1@users.noreply.github.com>
2024-10-06 18:23:45 +0000

    using gitcodespace

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { PetraWallet } from "petra-plugin-wallet-adapter";;
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const wallets = [new PetraWallet()];
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AptosWalletAdapterProvider plugins={wallets} autoConnect={true} >
+      <App />
+    </AptosWalletAdapterProvider> 
+  </React.StrictMode>,
 )
--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  address: { type: String, required: true, unique: true },
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
-  rewards: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model('User', userSchema);
